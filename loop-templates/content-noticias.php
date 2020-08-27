bad743bb889974dac606f0902ab79093b520f388
MaDevops <mathieu.dussault@gmail.com>
2020-08-27 12:56:14 +0200

    27/08/2020

--- loop-templates/content-noticias.php
+++ loop-templates/content-noticias.php
@@ -90,82 +90,109 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	?>
 
-							<div class="col-lg-12 cadreblanc" style="background-color: #F8F8F8">
+							
                                  <?php 
                                     $video = get_field('bande_annonce');
                                     if( $video ) { ?>
-								<div class="video-responsive"  style="background-color: #F8F8F8">
-									<!-- <iframe width="420" height="315" src="https://www.youtube.com/embed/xc446vOqXm8" frameborder="0" allowfullscreen ></iframe>		-->
-									<iframe width="640" height="564" src="<?php echo $video; ?>" frameborder="0" controls allowFullScreen mozallowfullscreen webkitAllowFullScreen></iframe>									
-								</div>
-                                
+                                <div class="col-lg-12 cadreblanc" style="background-color: #F8F8F8">
+									<div class="video-responsive"  style="background-color: #F8F8F8">
+										<!-- <iframe width="420" height="315" src="https://www.youtube.com/embed/xc446vOqXm8" frameborder="0" allowfullscreen ></iframe>		-->
+										<iframe width="640" height="564" src="<?php echo $video; ?>" frameborder="0" controls allowFullScreen mozallowfullscreen webkitAllowFullScreen></iframe>									
+									</div>
+                                </div> <!-- cadreblanc -->
                                 <?php } 
 
 
- 							$images = get_field('galerie');
-
-                                  if( $images ): 
-
                                 ?>
 						
-								<div class="gallery" <?php if(!($video)) {echo "style='padding-top:0;'";} ?> >
-									<div id="demo" class="carousel slide" data-ride="carousel">
-
-									  <!-- Indicators -->
-									 <!-- <ul class="carousel-indicators">
-										<li data-target="#demo" data-slide-to="0" class="active"></li>
-										<li data-target="#demo" data-slide-to="1"></li>
-										<li data-target="#demo" data-slide-to="2"></li>
-									  </ul>-->
-
-									  <!-- The slideshow -->
-									  <div class="carousel-inner">
-                                          
-                                        <?php  
-           	 	                          /* $i =1;
-                                          $postID = get_the_ID();
-                                             $gallery = get_post_gallery_images($postID);
-                                          if (!empty($gallery)){
-                                            foreach( $gallery as $image_url ){*/
-                                          $i =1;
-                                         // $images = get_field('galerie');
-
-                                        //    if( $images ): 
-                                          foreach( $images as $image ): ?>
-											<!-- <?php echo trim($image['url'], "?"); ?>-->
-										<div class="lol carousel-item <?php if ($i ==1) { echo "active"; $i++; } ?>">
-										  <img src="<?php echo $image['url']; ?>" alt="<?php the_title(); ?>"> 
-										</div>
-                                          
-                                        <?php endforeach;  ?>
-
-                                        <?php //endif; ?> 
-									  
-                                        </div>
-
-								
-									  <!-- Left and right controls -->
-									  <a class="carousel-control-prev" href="#demo" data-slide="prev">
-										<span class="carousel-control-prev-icon"></span>
-									  </a>
-									  <a class="carousel-control-next" href="#demo" data-slide="next">
-										<span class="carousel-control-next-icon"></span>
-									  </a>
- 							
-
-
-									</div>
-								</div>
-
-
- 						<?php endif; ?> 
-
-							</div> <!-- cadreblanc -->
+			
+							
 
 <?php //} // endif video image?>
 							
+
+<div class="col-lg-12 order-lg-last px-4 py-3">
+		<?php //the_posts_navigation(); ?>
+
+		<?php 
+
+		$post = get_post();
+		//print_r($post);
+		$id = $post->ID;
+		//$id = $post['ID'];
+		//print_r($id);
+
+
+		$categories = get_the_category();
+ 
+if ( ! empty( $categories ) ) {
+	$category = esc_html( $categories[0]->name );
+    //echo $category;
+}
+
+
+		/*$post_categories = wp_get_post_categories($id);
+		$cats = array();
+     	foreach($post_categories as $c){
+    		$cat = get_category( $c );
+    		$cats[] = array( 'name' => $cat->name, 'slug' => $cat->slug );
+		}
+		$category = $cats[0];
+		print_r($category);*/
+
+        // if (isset($_GET['category'])) {
+    	//	$category = $_GET['category'];
+   
+           // $prev = add_query_arg( 'category', $category, get_permalink(get_adjacent_post(true,'',false)) );
+          //  $next = add_query_arg( 'category', $category, get_permalink(get_adjacent_post(true,'',true)) );
+           
+		//} else {
+     	 	$prev = get_permalink(get_adjacent_post(true,'',false));
+      	  	$next = get_permalink(get_adjacent_post(true,'',true));  
+		//} 
+         
+         ?>
+
+        <div class="row">
+         	<div class="col-3">
+				<?php if ($prev != get_permalink() ) { ?>
+				
+				<a class="prev" href="<?php echo $prev; ?>" >
+		            <img src="<?php bloginfo('stylesheet_directory');?>/img/precedent.svg">   
+		            News précédente
+		            
+		        
+		        </a><?php } ?> 
+		    </div>
+
+		    <div class="col-6">
+		    	
+		    </div>
+
+
+		    <div class="col-3 text-right">
+				<?php if ($next != get_permalink() ) { ?>
+				<a class="next" href="<?php echo $next; ?>" >
+		        
+		            News suivante <img src="<?php bloginfo('stylesheet_directory');?>/img/suivant.svg">
+		                	 
+		        
+		        </a><?php } ?>
+		    </div>
+
+
+    	</div>
+
+	</div>
+
 						</div> <!-- row --> 
 						
+
+
+
+	
+
+
 					</div> <!-- col lg 9 -->
 					
 			
@@ -210,126 +237,20 @@ if(isset($_GET["email"])) { $email = $_GET["email"]; }
 
 
 								      <?php 
-		                                    $telechargements = get_field('telechargements');	
+		                                    $fiche = get_field('fiche_film');	
 		                                   // print_r($telechargements);
-		                                   if ($telechargements){
+		                                   if ($fiche){
 		                            ?>
 
 									<?php //echo $technique['format']; ?>
 
 						<div class="row">
 
-							<?php 
 
-
-							$ba = $telechargements['bande-annonce']; 
-							$affiche = $telechargements['affiche_hd']; 
-							$dp = $telechargements['dossier_de_presse']; 
-							$revue = $telechargements['revue_de_presse']; 
-							$photos = $telechargements['photos_hd']; 
-							$extraits = $telechargements['extraits']; 
-							$touslesdocuments = $telechargements['tous_les_documents']; 
-							//print_r($ba);
-							//$ba 
-							$hd = $ba['hd']; 
-							//print_r($hd);
-							$dcp = $ba['dcp'];
-							
-
-
-							?>
-                            <?php if( $hd || $dcp /*get_field('bande-annonce')*/ ){?>
-								<div class="col-6 col-md-4 col-lg-12">
-	                                <?php //the_field('bande-annonce'); ?>
-	                               <!-- <a href="#" alt="">-->
-	                                	<button class="download opensubmenuba">BANDE-ANNONCE</button>
-	                              <!--  </a>-->
-	                                <div class="submenuba animated fadeIn">
-	                                	<?php //print_r($hd);
-	                                		if ($hd){
-	                                	?>
-	                                	<a href="<?php echo $hd; //echo $srchd; ?>" target="blank">HD</a><br />
-	                                	<?php 		} // endif $hd
-	                                	 
-	                                	if ($dcp){
-	                                	?>
-	                                		<a href="<?php echo $dcp; ?>" target="blank">DCP</a>
-	                                	<?php }// endif  $dcp ?>
-	                                </div>
-								</div>
-                            <?php } // endif $hd ou $dcp ?>
-
-
-                             <?php if( $affiche /*get_field('affiche_hd')*/ ){?>
 							<div class="col-6 col-md-4 col-lg-12">
-								 <a href="<?php echo $affiche; //the_field('affiche_hd'); ?>" alt="" download><button class="download">AFFICHE HD</button></a>
+								 <a href="<?php echo $fiche; //the_field('affiche_hd'); ?>" alt=""><button class="download">VOIR LA FICHE DU FILM</button></a>
 							</div>
-                            <?php } ?>
-
-                             <?php if( $dp ){?>
-							<div class="col-6 col-md-4 col-lg-12">
-								 <a href="<?php echo $dp; ?>" alt="" download><button class="download">DOSSIER DE PRESSE</button></a>
-							</div>
-                            <?php } ?>  
-                            
-
-                             <?php if( $revue ){?>
-							<div class="col-6 col-md-4 col-lg-12">
-								 <a href="<?php echo $revue; ?>" alt="" download><button class="download">REVUE DE PRESSE</button></a>
-							</div>
-                            <?php } ?>  
-                            
-
-
-                             <?php if( $photos ){?>
-
-								<div class="col-6 col-md-4 col-lg-12">
-									 <a href="<?php echo $photos; ?>" alt="" download><button class="download">PHOTOS HD</button></a>
-								</div>
-
-
-
-                            <?php } ?>
-
-
-                             <?php if( $extraits ){?>
-                             <?php //if( have_rows('photos_hd') ): ?>
-							<div class="col-6 col-md-4 col-lg-12">
-								<!-- <a href="#" alt="">-->
-								 	<button class="download opensubmenuextraits">EXTRAITS</button>
-								<!-- </a>-->
-							
-									<div class="submenuextraits animated fadeIn">
-
-		  									 <?php 
-		  									 //print_r($photos);
-		  									foreach ($extraits as $extrait) {
-		  									 	$nom = $extrait['nom_de_lextrait'];
-		                                        $url = $extrait['fichier'];
-		  									
-	
-		                                        ?>
-		                                        
-		                                        <a href="<?php echo $url; ?>" target="blank"><?php echo $nom; ?></a><br />
-
-		                                    
-		                                <?php //endwhile; 
-			                                	 }
-										?>
-			                        </div>
-			                </div>
-                            <?php } ?>
-
-
-
-
-
-                            <?php if( $touslesdocuments ){?>
-								<div class="col-6 col-md-4 col-lg-12">
-									 <a href="<?php echo $touslesdocuments; ?>" alt="" download><button class="downloadall">TOUS LES DOCUMENTS</button></a>
-								</div>
-                            <?php } ?>
-
+                          
                            
 
 						</div>
@@ -337,11 +258,20 @@ if(isset($_GET["email"])) { $email = $_GET["email"]; }
 						<?php } // endif $telechargements ?>
 
 
-					</div>
+					</div> <!-- fin colonne droite -->
 
 
 	
-		</div>
+</div> <!-- end row -->
+
+<!--
+<div class="row">
+
 </div>
+-->
+
+
+
+	</div> <!-- end content container-fluid -->
 
 </div>
